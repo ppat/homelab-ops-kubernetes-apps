bf3ad4735e9907f0bb668dd926a165c42458face
Peter Pathirana <peter@latent.io>
2026-07-01 11:58:42 -0400

    chore(infra-networking-extra): drop unifi network controller as we have moved to gateway hosted unifi os (#3195)

--- infrastructure/subsystems/networking-extra/unifi/conf.d/init-mongo.js
+++
@@ -1,2 +0,0 @@
-db.getSiblingDB("unifi").createUser({ user: "${MONGO_USER}", pwd: "${MONGO_PASS}", roles: [{ role: "dbOwner", db: "unifi" }] });
-db.getSiblingDB("unifi_stat").createUser({ user: "${MONGO_USER}", pwd: "${MONGO_PASS}", roles: [{ role: "dbOwner", db: "unifi_stat" }] });
